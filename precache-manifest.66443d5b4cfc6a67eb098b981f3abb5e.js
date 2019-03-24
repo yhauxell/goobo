ce05af939ef39ace47d476616b5fae8b02cc0609
Yausell Ruiz <yhauxell@gmail.com>
2019-03-24 16:13:39 -0300

    Updates

---
+++ precache-manifest.66443d5b4cfc6a67eb098b981f3abb5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d208d3f99b2a989b8643add50c367b70",
+    "url": "/goobo/static/media/dead-error.d208d3f9.svg"
+  },
+  {
+    "revision": "52db4a9c2963c130f1a3",
+    "url": "/goobo/static/js/runtime~main.73fab527.js"
+  },
+  {
+    "revision": "3c7e99b69ab7e6fb9fed",
+    "url": "/goobo/static/js/main.3af8c2a3.chunk.js"
+  },
+  {
+    "revision": "c295d176de75b9886366",
+    "url": "/goobo/static/js/2.43d615a2.chunk.js"
+  },
+  {
+    "revision": "3c7e99b69ab7e6fb9fed",
+    "url": "/goobo/static/css/main.3a150999.chunk.css"
+  },
+  {
+    "revision": "7e9cdf6e8d28a4f902e1136e68bc0411",
+    "url": "/goobo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.794474a70641efd65e302aee32800e80.js"
+  "/goobo/precache-manifest.66443d5b4cfc6a67eb098b981f3abb5e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/goobo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
